80bb4b78c535ae58ec8039bb38a78ed2074622c4
swapnil <dasswapnil96@gmail.com>
2017-08-02 15:45:58 +0530

    File for plotting

---
+++ plot.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import hull
+
+fig = plt.figure() # For plotting
+ax = fig.add_subplot(111, projection='3d')
+
+for point in hull.final_vertices:
+	ax.scatter(point.x, point.y, point.z, c='b', marker='o')
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
